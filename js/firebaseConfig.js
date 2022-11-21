69c2afc49033ee810469abe3e78967a09e86d7af
Pranjal <pranjal.mishra_cs20@gla.ac.in>
2022-11-22 03:51:59 +0530

    First Commit

---
+++ js/firebaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBn4ba21LTBcESIXth7JoCjIKIbtKPM_Pg",
+  authDomain: "newlogin-62fc5.firebaseapp.com",
+  databaseURL: "https://newlogin-62fc5-default-rtdb.firebaseio.com",
+  projectId: "newlogin-62fc5",
+  storageBucket: "newlogin-62fc5.appspot.com",
+  messagingSenderId: "886716625391",
+  appId: "1:886716625391:web:911f92867c439611f2c61a",
+  measurementId: "G-Q12XWB8MSV"
+  };
+    firebase.initializeApp(firebaseConfig);
+  
